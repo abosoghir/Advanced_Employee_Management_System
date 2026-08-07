714885222b69cfba043c5fad0b2b808ed61f42ff
abosoghir <abosoghir2022@gmail.com>
2026-08-07 18:46:22 +0300

    Add project files.

---
+++ Advanced_Employee_Management_System/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Advanced Employee Management System!");
+        // Additional code for the application can be added here
+    }
+}
