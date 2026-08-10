2f0584d94a015c0e1bcc93c62a0bc5418d4c7560
abosoghir <abosoghir2022@gmail.com>
2026-08-10 20:45:56 +0300

    Add the event

---
+++ Advanced_Employee_Management_System/Events/EmployeeEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Advanced_Employee_Management_System.Models;
+
+namespace Advanced_Employee_Management_System.Events;
+
+public delegate void EmployeeEvent(EmployeeEventArgs employeeEventArgs);
+public class EmployeeEventArgs 
+{
+    public Employee Employee { get; }
+    public string Message { get; }
+    public EmployeeEventArgs(Employee employee, string message)
+    {
+        Employee = employee;
+        Message = message;
+    }
+}
--- Advanced_Employee_Management_System/Program.cs
+++ Advanced_Employee_Management_System/Program.cs
@@ -5,44 +5,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //public void run()
-        //{
-        //    while (true)
-        //    {
-        //        int choice = menu();
-
-        //        if (choice == 1)
-        //            AddDepartment();
-        //        else if (choice == 2)
-        //            AddEmployeeToOnboarding();
-        //        else if (choice == 3)
-        //            ProcessNextEmployee();
-        //        else if (choice == 4)
-        //            AddSkill();
-        //        else if (choice == 5)
-        //            SearchById();
-        //        else if (choice == 6)
-        //            SearchByName();
-        //        else if (choice == 7)
-        //            ShowDepartmentEmployees();
-        //        else if (choice == 8)
-        //            CalculateAverageSalaryInDepartment();
-        //        else if (choice == 9)
-        //            DepartmentReport();
-        //        else if (choice == 10)
-        //            ShowHistory();
-        //        else if (choice == 11)
-        //            ShowCompanySkills();
-        //        else if (choice == 12)
-        //            ShowEmployees();
-        //        else if (choice == 13)
-        //            ShowWaitingEmployees();
-        //        else
-        //            break;
-        //    }
-        //}
-
-
+        Company company = new Company();
 
     }
     private static void HandleFilterEmployees(Company company)
